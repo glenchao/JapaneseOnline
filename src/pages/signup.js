c147c08bd7f31e39e006958c29e1caeae2936ecd
Werner Chao <oahcrenrew@gmail.com>
2016-09-25 00:37:18 +0800

    added data flow of signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -12,6 +12,31 @@ class Signup extends Component {
     this.phoneId = generate();
     this.channelId = generate();
     this.otherlId = generate();
+    this.state = {
+        email: '',
+        phone: '',
+        name: '',
+        channel: '',
+        other: '',
+    }
+  }
+   handleChange (label) {
+        return function (e) {
+            let state = {};
+            state[label] = e.target.value;
+            this.setState(state);
+        }.bind(this);
+    }
+  getValidationState(label) {
+    const length = this.state["email"].length;
+    if (length > 5) return 'success';
+    else if (length > 3) return 'warning';
+    else if (length > 0) return 'error';
+  }
+  onSubmit(e) {
+      e.preventDefault();
+      alert(this.state.email);
+
   }
   render() {
     return (
@@ -23,49 +48,69 @@ class Signup extends Component {
         </Row>
         <Row>
             <Col xs={10} xsOffset={1} sm={6} smOffset={3}>
-                <form>
-                <FormGroup controlId={this.emailId}>
-                    <ControlLabel>Email</ControlLabel>
-                    <FormControl
-                    type="text"
-                    placeholder="Enter email"
-                    />
-                    <FormControl.Feedback />
-                </FormGroup>
-                <FormGroup controlId={this.nameId}>
-                    <ControlLabel>Name</ControlLabel>
-                    <FormControl
-                        type="text"
-                        placeholder="Enter Your Name"
-                    />
-                    <FormControl.Feedback />
-                </FormGroup>
-                <FormGroup controlId={this.phoneId}>
-                    <ControlLabel>Phone</ControlLabel>
-                    <FormControl
-                        type="number"
-                        placeholder="Phone Number"
-                    />
-                    <FormControl.Feedback />
-                </FormGroup>
-                <FormGroup controlId={this.channelId}>
-                    <ControlLabel>Channel</ControlLabel>
-                    <FormControl componentClass="select" placeholder="please select">
-                        <option value="google">Google</option>
-                        <option value="facebook">Facebook</option>
-                    </FormControl>
-                    <FormControl.Feedback />
-                </FormGroup>
-                <FormGroup controlId={this.otherId}>
-                    <ControlLabel>Other</ControlLabel>
-                    <FormControl
-                        componentClass="textarea"
-                        type="text"
-                        placeholder="What other channels did you hear about us?"
-                    />
-                    <FormControl.Feedback />
-                </FormGroup>
-                <Button>Submit</Button>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                        <FormGroup 
+                            controlId={this.emailId} 
+                            validationState={this.getValidationState()}
+                        >
+                            <ControlLabel>Email</ControlLabel>
+                            <FormControl
+                            type="text"
+                            placeholder="Enter email"
+                            value={this.state.email}
+                            onChange={this.handleChange('email')}
+                            />
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup 
+                            controlId={this.phoneId}
+                        >
+                            <ControlLabel>Phone</ControlLabel>
+                            <FormControl
+                                type="number"
+                                placeholder="Phone Number"
+                                value={this.state.phone}
+                                onChange={this.handleChange('phone')}
+                            />
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup controlId={this.nameId}>
+                            <ControlLabel>Name</ControlLabel>
+                            <FormControl
+                                type="text"
+                                placeholder="Enter Your Name"
+                                value={this.state.name}
+                                onChange={this.handleChange('name')}
+                            />
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup controlId={this.channelId}>
+                            <ControlLabel>Channel</ControlLabel>
+                            <FormControl 
+                                componentClass="select" 
+                                placeholder="please select"
+                                value={this.state.channel}
+                                onChange={this.handleChange('channel')}
+                            >
+                                <option value="google">Google</option>
+                                <option value="facebook">Facebook</option>
+                            </FormControl>
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup 
+                            controlId={this.otherId}
+                            value={this.state.other}
+                            onChange={this.handleChange('other')}
+                        >
+                            <ControlLabel>Other</ControlLabel>
+                            <FormControl
+                                componentClass="textarea"
+                                type="text"
+                                placeholder="What other channels did you hear about us?"
+                            />
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <Button type="submit">Submit</Button>
                 </form>
             </Col>
         </Row>
